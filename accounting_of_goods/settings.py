417cc959395ce4c67e4bae7401cdac6690929916
tm.kk23 <mansur.tumashev@gmail.com>
2023-04-12 12:37:21 +0500

    Added goods to installed apps

--- accounting_of_goods/settings.py
+++ accounting_of_goods/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 ]
 
+INSTALLED_APPS += [
+    'goods'
+]
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
